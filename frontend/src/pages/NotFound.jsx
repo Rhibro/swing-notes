7899cedacf29da42984ceb9b8523fd201179aa0a
Rhibro <rhibron@gmail.com>
2025-06-10 15:41:09 +0200

    not found content added

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router";
+
 const NotFound = () => {
-  return <h1>404</h1>;
+  return (
+    <>
+      <h1>404</h1>
+      <h1>Not Found</h1>
+      <p>Looks like this page isn't available...</p>
+      <button>
+        <Link className="no-underline" to="/login">Login</Link>
+      </button>
+    </>
+  ) 
 };
 
 export default NotFound;
